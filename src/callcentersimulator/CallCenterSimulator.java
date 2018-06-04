68e6a9f976fad568fc23bafc4a5ad0f410f006cc
JoshTan <kaiweitan128@gmail.com>
2018-06-04 10:46:49 +0800

    Call Center Simulator Version 1.0

--- src/callcentersimulator/CallCenterSimulator.java
+++ src/callcentersimulator/CallCenterSimulator.java
@@ -31,12 +31,12 @@ public class CallCenterSimulator {
         
         Scanner reader = new Scanner(System.in);
 
-        System.out.println("------------------------------------------------------------------------");
-        System.out.println("                 >>> ATM QUEUE SIMULATION SYSTEM <<<                    ");
-        System.out.println("------------------------------------------------------------------------"); 
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("             >>> CALL CENTER QUEUE SIMULATION SYSTEM <<<                    ");
+        System.out.println("----------------------------------------------------------------------------"); 
         
         System.out.print("Please enter number of service agents: ");      
-        while (serviceAgentQty < 0) {
+        while (serviceAgentQty <= 0) {
             try {
                 serviceAgentQty = Integer.parseInt(reader.nextLine());
             } catch (NumberFormatException e) {
@@ -44,14 +44,14 @@ public class CallCenterSimulator {
                 System.out.print("Please enter number of service agents: ");
                 continue;
             }        
-            if (serviceAgentQty < 0){
-                System.out.println("Please enter a positive integer!\n");
+            if (serviceAgentQty <= 0){
+                System.out.println("Please enter a positive integer and integer more than 0!\n");
                 System.out.print("Please enter number of service agents: ");
             }            
         }
         
         System.out.print("Please enter simulation time (in minutes): ");
-        while (minutes < 0) {
+        while (minutes <= 0) {
             try {
                 minutes = Integer.parseInt(reader.nextLine());
             } catch (NumberFormatException e) {
@@ -59,8 +59,8 @@ public class CallCenterSimulator {
                 System.out.print("Please enter simulation time (in minutes): ");
                 continue;
             }        
-            if (minutes < 0){
-                System.out.println("Please enter a positive integer!\n");
+            if (minutes <= 0){
+                System.out.println("Please enter a positive integer and integer more than 0!\n");
                 System.out.print("Please enter simulation time (in minutes): ");
             }            
         }
